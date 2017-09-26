36b54dc39e6763643d125009cd833b3fb295b9a8
dodo <dorian.cuquemelle@hotmail.fr>
2017-09-26 16:02:22 +0200

    commentaires tmtc

--- AI-fundamentals/assign1-robot/src/model/quaternion.py
+++ AI-fundamentals/assign1-robot/src/model/quaternion.py
@@ -4,9 +4,22 @@ from .vector import Vector
 
 
 class Quaternion:
+    """
+    Class that represents quaternions with 4 float numbers.
+    Implements useful functions such as heading, rotations...
+    """
     __w = 0
 
     def __init__(self, w, vector):
+        """
+        Initialization of the Quaternion class given a float and a Vector
+        :param tar_pos: targeted position to travel to
+        :type tar_pos: Vector
+        :param cur_pos: current position of the robot
+        :type cur_pos: Vector
+        :param cur_rot: current rotation of the robot
+        :type cur_rot: Quaternion
+        """
         if not isinstance(vector, Vector):
             raise (TypeError('Parameter vector of init is not a vector'))
         self.__unit_vector = vector
@@ -14,15 +27,10 @@ class Quaternion:
 
     def __mul__(self, other):
         """
-                elif isinstance(other, Vector):
-                    axis = Vector(self.x, self.y, self.z)
-                    uv = axis * other
-                    uuv = axis * uv
-                    uv *= (2.0 * self.__w)
-                    uuv *= 2.0
-                    return other + uv + uuv
-                """
-
+        multiplies the Quaternion with another, in that order
+        :param other: the other quaternion for operation
+        :type other: Quaternion
+        """
         if isinstance(other, Quaternion):
             return Quaternion(self.w * other.w - self.x * other.x - self.y * other.y - self.z * other.z,
                               Vector(self.__w * other.x + self.x * other.w + self.y * other.z - self.z * other.y,
@@ -32,9 +40,17 @@ class Quaternion:
             raise TypeError('trying to multiply a quaternion by something else than a quaternion or a vector')
 
     def __rmul__(self, other):
+        """
+        multiplies the Quaternion with another, in that order
+        :param other: the other quaternion for operation
+        :type other: Quaternion
+        """
         return self.__mul__(other)
 
     def __str__(self):
+        """
+        Computes a string giving information about the quaternion
+        """
         return "Quaternion<{}, [{},{},{}]>".format(self.w, self.x, self.y, self.z)
 
     @property
@@ -68,16 +84,30 @@ class Quaternion:
         return Quaternion(dict['W'], Vector(dict['X'], dict['Y'], dict['Z']))
 
     def normalize(self):
+        """
+        Returns norm of quaternion
+        """
         return (self.__w + self.x + self.y + self.z) / \
                numpy.sqrt(self.__w * self.__w + self.x * self.x + self.y * self.y + self.z * self.z)
 
     def conjugate(self):
+        """
+        Returns conjugate of quaternion
+        """
         return Quaternion(self.w, Vector(-self.x, -self.y, -self.z))
 
     def heading(self):
+        """
+        Returns the Vector heading from the robot
+        """
         return self.rotate(Vector.x_forward())
 
     def rotate(self, v):
+        """
+        Computes the rotation of a Vector v according to the Quaternion
+        :param v: vector to rotate
+        :type v: Vector
+        """
         rotated = (self * Quaternion(0, Vector(v.x, v.y, v.z))) * self.conjugate()
         return Vector(rotated.x, rotated.y, rotated.z)
 
