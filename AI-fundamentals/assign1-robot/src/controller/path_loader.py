114203a6e451c9892a99b70c8143f791da44b8ce
CyrilBos <bos.cyril33@gmail.com>
2017-09-27 12:15:10 +0200

    deleted timestamps and comments

--- AI-fundamentals/assign1-robot/src/controller/path_loader.py
+++ AI-fundamentals/assign1-robot/src/controller/path_loader.py
@@ -3,6 +3,10 @@ from model import Vector, Quaternion
 
 
 class PathLoader:
+    """
+    Class that loads the path files into lists of Vector used by our program or dictionaries used by the unit tests.
+    """
+
     def __init__(self, filename):
         # Load the path from a file and convert it into a list of coordinates
         self.loadPath(filename)
@@ -14,22 +18,29 @@ class PathLoader:
 
         self.path = data
 
-    def positionPath(self, dict=False, timestamps=False):
+    def positionPath(self, dict=False):
+        """
+        Parses the positions in the loaded file into a list of either Vector instances or dictionaries depending on the
+        value of the dict parameter.
+        :param dict: if set to True, will parse into dictionaries instead of into Vector instances
+        :type dict: bool
+        """
         if dict:
             return [{'X': p['Pose']['Position']['X'],
                      'Y': p['Pose']['Position']['Y'],
                      'Z': p['Pose']['Position']['Z']}
                     for p in self.path]
         else:
-            if timestamps:
-                return [(Vector.from_dict(p['Pose']['Position']), p['Timestamp'])
-                        for p in self.path]
-            else:
-                return [Vector.from_dict(p['Pose']['Position'])
-                        for p in self.path]
-
+            return [Vector.from_dict(p['Pose']['Position'])
+                    for p in self.path]
 
     def orientationPath(self, dict=False):
+        """
+        Parses the orientations in the loaded file into a list of either Quaternion instances or dictionaries depending on the
+        value of the dict parameter.
+        :param dict: if set to True, will parse into dictionaries instead of into Quaternion instances
+        :type dict: bool
+        """
         if dict:
             return [{'W': p['Pose']['Orientation']['W'],
                      'X': p['Pose']['Orientation']['X'],
