494a29df982317b94e89d0d676e39e188d14cf2b
CyrilBos <bos.cyril33@gmail.com>
2017-10-20 14:45:14 +0200

    removed useless code

--- AI-fundamentals/assign2-neural/src/faces.py
+++ AI-fundamentals/assign2-neural/src/faces.py
@@ -1,7 +1,4 @@
-import logging
-
 import sys
-
 
 from utils import Parser, image_preprocessing
 from perceptron import Perceptron
@@ -15,22 +12,8 @@ if __name__ == '__main__':
     parser = Parser()
     training_images = parser.parse_training_images(training_path, facit_path)
 
-    """   
-    for image in training_images:
-        row_count = len(training_images[0].pixels)
-        if row_count != 20:
-            raise Exception('Parsing problem. Image with {} rows of pixels instead of 20'.format(row_count))
-        else:
-            for row in training_images[0].pixels:
-                row_pixels_count = len(row)
-                if row_pixels_count != 20:
-                    raise Exception(
-                        'Parsing problem. Image with a row of {} pixels instead of 20'.format(row_pixels_count))
-    """
-
     image_preprocessing.blur_images(training_images)
     image_preprocessing.rotate_images(training_images)
-
 
     perceptron = Perceptron(training_images)
 
@@ -38,6 +21,5 @@ if __name__ == '__main__':
 
     image_preprocessing.blur_images(test_images)
     image_preprocessing.rotate_images(test_images)
-
 
     perceptron.classify_images(test_images)
